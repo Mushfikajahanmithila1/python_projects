94b270617caabdd6dad86746f9f2bc164badbb3a
mushfika <musfika962@gmail.com>
2026-09-24 15:19:31 +0600

    take

---
+++ 04.screen-recording.py
@@ -0,0 +1,29 @@
+# step 1
+import pyautogui 
+import cv2
+import numpy as np
+
+# step 2
+resolution = (1920, 1080)
+codec = cv2.VideoWriter_fourcc(*"XVID")
+filename = "Recording.mp4"
+fps = 60.0
+out = cv2.VideoWriter(filename, codec, fps, resolution)
+
+# step 3
+cv2.namedWindow("Live", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Live", 480, 270)
+
+# step 4
+while True:
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    out.write(frame)
+    cv2.imshow('Live', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# step 5
+out.release()
+cv2.destroyAllWindows()
